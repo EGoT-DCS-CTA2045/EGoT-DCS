0cfdf3e866ea8e2db5f7c547f9eabb9a36a83383
blusky-cloud <sspitzer@pdx.edu>
2021-03-08 20:42:52 -0800

    modify dcm.cpp to include sample2 logic

--- src/DCM.cpp
+++ src/DCM.cpp
@@ -6,7 +6,123 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
-    CTA2045Translator tr;
-    tr.connect();
-    return 0;
+    MSTimer timer;
+	bool shutdown = false;
+
+	CEA2045SerialPort sp("/dev/ttyUSB0");
+	UCMImpl ucm;
+	ResponseCodes responseCodes;
+
+	if (!sp.open())
+	{
+		LOG(ERROR) << "failed to open serial port: " << strerror(errno);
+		return 0;
+	}
+
+	ICEA2045DeviceUCM *device = DeviceFactory::createUCM(&sp, &ucm);
+
+	device->start();
+
+	timer.reset();
+
+	responseCodes = device->querySuportDataLinkMessages().get();
+
+	LOG(INFO) << "  query data link elapsed time: " << timer.getElapsedMS();
+
+	timer.reset();
+
+	responseCodes = device->queryMaxPayload().get();
+
+	LOG(INFO) << "  query max payload elapsed time: " << timer.getElapsedMS();
+
+	timer.reset();
+
+	responseCodes = device->querySuportIntermediateMessages().get();
+
+	LOG(INFO) << "  query intermediate elapsed time: " << timer.getElapsedMS();
+
+	timer.reset();
+
+	responseCodes = device->intermediateGetDeviceInformation().get();
+
+	LOG(INFO) << "  device info elapsed time: " << timer.getElapsedMS();
+
+	LOG(INFO) << "startup complete";
+
+	while (!shutdown)
+	{
+		char c = getchar();
+
+		switch (c)
+		{
+			case 'c':
+				device->basicCriticalPeakEvent(5).get();
+				break;
+
+			case 'e':
+				device->basicEndShed(0).get();
+				break;
+
+			case 'g':
+				device->basicGridEmergency(5).get();
+				break;
+
+			case 'l':
+				device->basicLoadUp(5).get();
+				break;
+
+			case '\n':
+				break;
+
+			case 'n':
+				device->basicNextRelativePrice(153).get(); // approx 4x
+				break;
+
+			case 'o':
+				device->basicOutsideCommConnectionStatus(OutsideCommuncatonStatusCode::Found);
+				break;
+
+			case 'p':
+				device->basicPowerLevel(63).get();		// approx 50%
+				break;
+
+			case 'q':
+				shutdown = true;
+				break;
+
+			case 'r':
+				device->basicPresentRelativePrice(101).get();	// approx twice
+				break;
+
+			case 's':
+				device->basicShed(5).get();
+				break;
+
+			case 'C':
+				device->intermediateGetCommodity().get();
+				break;
+
+			case 'O':
+				device->intermediateGetTemperatureOffset().get();
+				break;
+
+			case 'S':
+				device->intermediateGetSetPoint().get();
+				break;
+
+			case 'T':
+				device->intermediateGetPresentTemperature().get();
+				break;
+
+			default:
+				LOG(WARNING) << "invalid command";
+				break;
+		}
+	}
+
+	device->shutDown();
+
+	delete (device);
+
+	return 0;
 }
