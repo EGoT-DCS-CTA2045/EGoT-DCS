a47edb0fd494ac52b4f34edd806176d93cde246a
blusky-cloud <sspitzer@pdx.edu>
2021-04-21 14:33:57 -0700

    more gprof sample2 test2 run output

---
+++ _deps/sep-build/model/CMakeFiles/Model.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Model.dir/active_power.cpp.o"
+  "CMakeFiles/Model.dir/date_time_interval.cpp.o"
+  "CMakeFiles/Model.dir/der_control.cpp.o"
+  "CMakeFiles/Model.dir/der_control_base.cpp.o"
+  "CMakeFiles/Model.dir/end_device_control.cpp.o"
+  "CMakeFiles/Model.dir/event.cpp.o"
+  "CMakeFiles/Model.dir/event_status.cpp.o"
+  "CMakeFiles/Model.dir/fixed_var.cpp.o"
+  "CMakeFiles/Model.dir/flow_reservation_request.cpp.o"
+  "CMakeFiles/Model.dir/flow_reservation_response.cpp.o"
+  "CMakeFiles/Model.dir/freq_droop_type.cpp.o"
+  "CMakeFiles/Model.dir/offset.cpp.o"
+  "CMakeFiles/Model.dir/per_cent.cpp.o"
+  "CMakeFiles/Model.dir/power_factor_with_excitation.cpp.o"
+  "CMakeFiles/Model.dir/randomizable_event.cpp.o"
+  "CMakeFiles/Model.dir/reactive_power.cpp.o"
+  "CMakeFiles/Model.dir/request_status.cpp.o"
+  "CMakeFiles/Model.dir/resource.cpp.o"
+  "CMakeFiles/Model.dir/respondable_resource.cpp.o"
+  "CMakeFiles/Model.dir/respondable_subscribable_identified_object.cpp.o"
+  "CMakeFiles/Model.dir/set_point.cpp.o"
+  "CMakeFiles/Model.dir/signed_per_cent.cpp.o"
+  "CMakeFiles/Model.dir/signed_real_energy.cpp.o"
+  "CMakeFiles/Model.dir/subscribable_resource.cpp.o"
+  "CMakeFiles/Model.dir/target_reduction.cpp.o"
+  "CMakeFiles/Model.dir/version_information.cpp.o"
+  "libModel.a"
+  "libModel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ xml_writer/CMakeFiles/announcer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/EGoT-DCS/xml_writer/abstract_command.cpp" "/home/pi/Documents/EGoT-DCS/xml_writer/CMakeFiles/announcer.dir/abstract_command.cpp.o"
+  "/home/pi/Documents/EGoT-DCS/xml_writer/announce_xml.cpp" "/home/pi/Documents/EGoT-DCS/xml_writer/CMakeFiles/announcer.dir/announce_xml.cpp.o"
+  "/home/pi/Documents/EGoT-DCS/xml_writer/main.cpp" "/home/pi/Documents/EGoT-DCS/xml_writer/CMakeFiles/announcer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/cea2045-src/cea2045"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
